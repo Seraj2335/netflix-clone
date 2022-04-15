085dfea59a079ef875a2f0f1114c78e6b69c1bf7
Seraj ahmad <kseraj96@gmail.com>
2022-04-15 10:56:47 +0530

    initial commit

--- src/firstComponent.jsx
+++ src/firstComponent.jsx
@@ -8,8 +8,15 @@ import Row from './row';
             return (
                <div>
                   
-                   <Row title="Netflix Originals" fetchUrl={`https://api.themoviedb.org/3${requests.fetchNetflixOriginal}`}/>
+                   <Row title="Netflix Originals" isLargeRow fetchUrl={`https://api.themoviedb.org/3${requests.fetchNetflixOriginal}`}/>
                    <Row title="Netflix Trending" fetchUrl={`https://api.themoviedb.org/3${requests.fetchTrending}`}/>
+                   <Row title="Top Rated" fetchUrl={`https://api.themoviedb.org/3${requests.fetchTopRated}`}/>
+                   <Row title="Action Movies" fetchUrl={`https://api.themoviedb.org/3${requests.fetchActionMovies}`}/>
+                   <Row title="Comedy Movies" fetchUrl={`https://api.themoviedb.org/3${requests.fetchComedyMovies}`}/>
+                   <Row title="Horror Movies" fetchUrl={`https://api.themoviedb.org/3${requests.fetchHorrorMovies}`}/>
+                   <Row title="Romance Movies" fetchUrl={`https://api.themoviedb.org/3${requests.fetchRomanceMovies}`}/>
+                   <Row title="Documentaries" fetchUrl={`https://api.themoviedb.org/3${requests.fetchDocumentaries}`}/>
+                   
                </div>
             );
         }
--- src/row.js
+++ src/row.js
@@ -2,7 +2,7 @@
 import React, { useState,useEffect} from 'react';
 import axios from 'axios';
 
-function Row({title,fetchUrl}){
+function Row({title,fetchUrl,isLargeRow}){
   const[movies,setMovies]=useState([]);
  const baseUrl ='https://image.tmdb.org/t/p/original/';
       useEffect(()=>{
@@ -18,12 +18,12 @@ function Row({title,fetchUrl}){
 
     
     return (
-       <div className='w-full object-contain'>
-         <h1 className='text-smr3 text-2xl font-semibold'>{title}</h1>
-         <div className='flex mt-2 overflow-scroll  overflow-y-hidden '>
+       <div className=' object-contain px-5 bg-black'>
+         <h1 className=' text-2xl font-bold'>{title}</h1>
+         <div className='flex my-4 overflow-scroll scrollbar-hide overflow-y-hidden px-5'>
            {
              movies.map(e=>{
-               return <img  src={`${baseUrl+e.poster_path}`} alt={e.name} className="h-25 transition transform hover:scale-105"/>
+               return <img key={e.id} src={`${baseUrl+e.poster_path}`} alt={e.name} className={`${isLargeRow ?'mr-2.5 w-full max-h-25 ':'h-25 w-40'} transition transform hover:scale-110`}/>
              })
            }
          </div>
